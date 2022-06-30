9cfb313105a76a71bae683a21dde055fb316e5b0
automotionShmuel <shmuelw@automotionparking.com>
2022-06-30 15:03:21 -0400

    removed unused imports

--- client/src/components/GarageCard/GarageCard.js
+++ client/src/components/GarageCard/GarageCard.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import ReportCard from "../ReportCard/ReportCard";
 import { useNavigate } from "react-router-dom";
 
 function GarageCard(props) {
 
-    const {id, title, image} = props;
+    const {title, image} = props;
    
     const urlParam = title.split(" ")[0];
     const nav = useNavigate();
@@ -20,7 +19,6 @@ function GarageCard(props) {
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text>
-                
                 </Card.Text>
                 <Button variant="primary" onClick={clickHandler}>Select Reports</Button>
             </Card.Body>
