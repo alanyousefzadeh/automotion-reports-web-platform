cf8f7ee2c4f0d97c89ac1d47f5fa9370628e0324
automotionShmuel <shmuelw@automotionparking.com>
2022-07-25 13:25:09 -0400

    adding totals

--- client/src/components/TransactionTable/TransactionTable.js
+++ client/src/components/TransactionTable/TransactionTable.js
@@ -9,13 +9,22 @@ function TransactionTable(props) {
     } = props;
 
     let listElements = []
+
     let times = ['12 AM', '1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM', '8 AM', '9 AM','10 AM', '11 AM', '12 PM',
     '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM', '9 PM','10 PM', '11 PM'
     ]
+
     for(let i = 0; i<24;  i++ ) {
         listElements.push([transientInTable[i], transientOutTable[i], monthlyInTable[i], monthlyOutTable[i]])
     } 
+
     console.log(listElements)
+
+    const monthlyInTotal = monthlyInTable.reduce((prevVal, currVal) => prevVal + currVal, 0)
+    const monthlyOutTotal = monthlyOutTable.reduce((prevVal, currVal) => prevVal + currVal, 0)
+    const transientInTotal = transientInTable.reduce((prevVal, currVal) => prevVal + currVal, 0)
+    const transientOutTotal = transientOutTable.reduce((prevVal, currVal) => prevVal + currVal, 0)
+
     return(
         
             <Table striped bordered size='sm'>
@@ -44,6 +53,14 @@ function TransactionTable(props) {
                     )
                 })
             }
+                <tr className='table-warning'>
+                    <th>Total</th>
+                    <th>{transientInTotal}</th>
+                    <th>{transientOutTotal}</th>
+                    <th>{monthlyInTotal}</th>
+                    <th>{monthlyOutTotal}</th>
+                    <th>{transientInTotal+transientOutTotal+monthlyInTotal+monthlyOutTotal}</th>
+                </tr>
             </tbody>
             </Table>
     )
