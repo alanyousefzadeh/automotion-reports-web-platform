038f3b56ce2d76cd0e9ce4678a5fd83242419f45
automotionShmuel <shmuelw@automotionparking.com>
2022-08-16 00:24:23 -0400

    adding cors code

--- server/index.js
+++ server/index.js
@@ -18,7 +18,8 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+
   )
   if (req.method === "OPTIONS") {
     res.header(
@@ -28,7 +29,8 @@ app.use((req, res, next) => {
     return res.status(200).json({})
   }
   next()
-})/////
+})
+/////end cors
 app.use(express.json());
 
 //Routes
