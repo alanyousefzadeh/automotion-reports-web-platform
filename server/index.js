2daf1927f6d9b0474405a4e2303ff18e2d56f598
automotionShmuel <shmuelw@automotionparking.com>
2022-08-16 09:45:17 -0400

    cors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 // const axios = require("axios");
 
 const loginRoute = require('./routes/loginRoute');
@@ -13,24 +13,24 @@ const retrievalTimeRoute = require('./routes/retrievalTimeRoute')
 const port = process.env.PORT || 8080;
 
 //middleware
-// app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
 
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
-/////end cors
+//   )
+//   if (req.method === "OPTIONS") {
+//     res.header(
+//       "Access-Control-Allow-Methods",
+//       "POST, PUT, PATCH, GET, DELETE"
+//     )
+//     return res.status(200).json({})
+//   }
+//   next()
+// })
+/////end cors////
 app.use(express.json());
 
 //Routes
@@ -39,6 +39,7 @@ app.use('/garagedata', garageRoute);
 app.use('/emailGenerator', emailRoute);
 app.use('/currentUser', currentUserRoute);
 app.use('/retrievalTime', retrievalTimeRoute);
+
 //port listener
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
