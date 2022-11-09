791d57a19ebc3e7eacf55be23edf17276e80f9cf
unknown <shmuelw@automotionparking.com>
2022-11-09 07:13:17 -0500

    adding service key for firebase server setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 require('dotenv').config();
+const { initializeApp } = require('firebase-admin/app');
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const axios = require("axios");
+
+/////////////////
+var admin = require("firebase-admin");
+
+//var serviceAccount = (process.env.SERVICE_KEY);
+
+admin.initializeApp({
+  credential: admin.credential.cert('./service_key.json'),
+  databaseURL: "https://automotion-web-platform-default-rtdb.firebaseio.com"
+});
+
+
 
 const loginRoute = require('./routes/loginRoute');
 const garageRoute = require('./routes/garageRoute');
@@ -30,6 +42,28 @@ app.use('/atlanticOpenAPI', atlanticOpenAPIRoute)
 app.use('/schemehorn', schemehornAPIRoute)
 app.use('/filterByRate', filterByRateRoute)
 app.use('/monthlies', monthliesRoute)
+app.use('/admindelete', (req, res) =>{
+
+  const listAllUsers = (nextPageToken) => {
+    // List batch of users, 1000 at a time.
+    admin.auth()
+      .listUsers(1000, nextPageToken)
+      .then((listUsersResult) => {
+        listUsersResult.users.forEach((userRecord) => {
+          console.log('user', userRecord.toJSON());
+        });
+        if (listUsersResult.pageToken) {
+          // List next batch of users.
+          listAllUsers(listUsersResult.pageToken);
+        }
+      })
+      .catch((error) => {
+        console.log('Error listing users:', error);
+      });
+  };
+  // Start listing users from the beginning, 1000 at a time.
+  listAllUsers();
+})
 
 //port listener
 app.listen(port, () => {
