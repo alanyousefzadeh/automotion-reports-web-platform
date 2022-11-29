c08ff3e293d787b713842f238847395d3d88fcb8
alanyousef <103208852+alanyousef@users.noreply.github.com>
2022-11-29 15:55:34 -0500

    initial setup

--- client/src/components/EmailComponent/EmailComponent.js
+++ client/src/components/EmailComponent/EmailComponent.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React , {useState} from 'react'
 import {UserAuth} from "../Context/AuthContext";
 import './EmailComponent.scss'
 import Button from "react-bootstrap/Button";
+import {base64Email} from "../EmailSender/EmailPdf";
 
 export default function EmailComponent() {
-  return (
-    <div>
-      <form className="emailForm">
-        <p className='emailForm_text'>From: {UserAuth().user.email}</p>
-        <p className='emailForm_text'>To: <input className='emailForm_to' type="text" placeholder="Enter email address" /></p>
-        <textarea className='emailForm_text_input'name="comment" placeholder='Add message...'></textarea>
-        <Button className="emailForm_button">
-            Send Report as Email
-        </Button>
-      </form>
-    </div>
-  );
+    const [to , setTo] = useState("")
+    const [body , setBody] = useState("")
+    const currentUser = UserAuth().user.email
+    return (
+        <div>
+            <form className="emailForm">
+                <p className='emailForm_text'>From: {currentUser}</p>
+                <p className='emailForm_text'>To: <input onChange={e=>setTo(e.target.value)} value={to} className='emailForm_to' type="text" placeholder="Enter email address" /></p>
+                <textarea onChange={e=>setBody(e.target.value)} value={body} className='emailForm_text_input'name="comment" placeholder='Add message...'></textarea>
+                <Button onClick={()=>base64Email(currentUser,to,body)} className="emailForm_button">
+                    Send Report as Email
+
+                </Button>
+            </form>
+        </div>
+    );
 }
--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -1,10 +1,13 @@
 const emailSend = require('../emailTest');
+const {request} = require("express");
 
 exports.sendEmail = (req, res) =>{
-    console.log(req.body.file)
-    let fileName = req.body.file.replace(/:\s*/g, "_");
-    console.log('fileName', fileName)
-    emailSend(fileName);
+    // console.log(req.body.dataUri)
+    // let fileName = req.body.dataUri.replace(/:\s*/g, "_");
+    // console.log('fileName', fileName)
+    let from = req.body.from;
+    let to = req.body.to;
+    let text = req.body.body;
+    emailSend(req.body.dataUri,from , to , text);
     // res.send("")
-}; 
-
+};
