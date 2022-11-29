48ba079f90b05008fb26d53b45c5c821cc133c92
unknown <shmuelw@automotionparking.com>
2022-11-28 20:54:51 -0500

    explicit url routing in filter by rate page

--- client/src/pages/AutomatedFilteredByRate/AutomatedFilteredByRate.js
+++ client/src/pages/AutomatedFilteredByRate/AutomatedFilteredByRate.js
@@ -31,7 +31,7 @@ export default function AutomatedFilteredByRate() {
             const token = sessionStorage.getItem('token');
             setLoading(true)
             const promise = await axios
-                .get(process.env.REACT_APP_FILTER_BY_RATE_URL, {
+                .get("https://automotion-heroku-server.herokuapp.com/filterByRate", {
                     params: {
                         rate, startDate, endDate, garageName
                     },
